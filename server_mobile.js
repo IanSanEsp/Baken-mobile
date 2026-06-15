f5209d6bc7ff514ee62fc8b79ba9dc36a7857592
Ian <isancheze2300@alumno.ipn.mx>
2026-06-15 15:03:24 -0600

    Update server_mobile.js

--- server_mobile.js
+++ server_mobile.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mysql   = require('mysql2/promise');
 const cors    = require('cors');
-const bcrypt  = require('bcrypt');
+const bcrypt  = require('bcryptjs');
 const jwt     = require('jsonwebtoken');
 require('dotenv').config();
 
